ebec091445fdeeeef127a0bbd6ebd3aaf5bfa646
kirilla <kg.workspaces@gmail.com>
2022-07-13 21:58:14 +0300

    Add functional for slick slider

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -3019,13 +3019,24 @@
 
 $(function() {
 
-    console.log(1);
-    $("#portfolio").slick({
-        arrows: true,
+    const portfolio = $("#portfolio")
+    
+    portfolio.slick({
+        arrows: false,
         dots: true,
         speed: 800,
         slidesToShow: 2,
         slidesToScroll: 2
     });
+    
+    $("#portfolioArrowPrev").on("click", function(event) {
+        event.preventDefault()
+        portfolio.slick("slickPrev")
+    })
+    
+    $("#portfolioArrowNext").on("click", function(event) {
+        event.preventDefault()
+        portfolio.slick("slickNext")
+    })
 
 });
